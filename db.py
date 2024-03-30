9073c2231ddf24e33e33121d5299eb9c6418f205
Annihilator007 <123206937+Annihilator007@users.noreply.github.com>
2024-03-30 14:37:26 +0530

    db.py

--- db.py
+++ db.py
@@ -1,12 +1,15 @@
 import mysql.connector
 
 # Database connection
+# for mysql use name run query( SELECT USER(); )
+# -- This query will return a result like 'username'@'hostname'.
 conn = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="Ab@6367802080",
+    user="your_my_sql_server_ussername",
+    password="your_my_sql_server_password",
     database="gym_management_system"
 )
+
 
 if conn.is_connected():
     print("Connected to the database")
